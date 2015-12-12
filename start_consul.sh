aa2e317ac3d795993fe483e6574f746ae57af2b9
Michael Hernandez <michael.hernandez1988@gmail.com>
2015-12-11 20:08:40 -0500

    add-start-scripts-and-maestro-sub-repo

---
+++ start_consul.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# run consul with the ui flag
+docker run -d -p 8400:8400 -p 8500:8500 -p 8600:53/udp -h node1 progrium/consul -server -bootstrap -ui-dir /ui
---
+++ start_maestro.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t miketheprogrammer/maestro ./maestro/; docker run -it miketheprogrammer/maestro /bin/bash
