4075e7be114b14c52fad9b48ea056e5a79786389
Michael Hernandez <michael.hernandez1988@gmail.com>
2015-12-11 22:33:26 -0500

    Increase mem of machine and new start_maestro

--- init_machines.sh
+++ init_machines.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker-machine create -d virtualbox --virtualbox-memory=1028 --virtualbox-cpu-count=4 $1;docker-machine env $1; eval $(docker-machine env $1);
+docker-machine create -d virtualbox --virtualbox-memory=2048 --virtualbox-cpu-count=4 $1;docker-machine env $1; eval $(docker-machine env $1);
 alias d='docker rm -f $(docker ps -a -q);docker rmi -f $(docker images -a -q)'
--- start_maestro.sh
+++ start_maestro.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker build -t miketheprogrammer/maestro ./maestro/; docker run -it miketheprogrammer/maestro /bin/bash
+#docker build --no-cache -t miketheprogrammer/maestro ./maestro/; docker run -t --net=host miketheprogrammer/maestro
+docker build --no-cache -t miketheprogrammer/maestro ./maestro/; docker run -it --volume=/var/run/docker.sock:/var/run/docker.sock --env ENGINE_LOCAL=true --net=host miketheprogrammer/maestro
