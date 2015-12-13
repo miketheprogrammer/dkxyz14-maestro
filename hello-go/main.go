ae79c66450eee9c5bb00b755f50fe8b10f18ff95
Michael Hernandez <michael.hernandez1988@gmail.com>
2015-12-13 09:58:34 -0500

    add-go-thrust

--- hello-go/main.go
+++ hello-go/main.go
@@ -1,27 +1,28 @@
 package main
 
 import (
-  "io"
-  "net/http"
-  "log"
-  "os"
+	"io"
+	"log"
+	"net/http"
+	"os"
 )
 
 // hello world, the web server
 func HelloServer(w http.ResponseWriter, req *http.Request) {
-  msg := os.Getenv("MSG")
-  if msg == "" {
-    msg = "hello world!\n"
-  }
-  io.WriteString(w, msg)
+	msg := os.Getenv("MSG")
+	if msg == "" {
+		msg = "hello world!\n"
+	}
+	io.WriteString(w, msg)
 }
 
 func main() {
-  log.Print("starting on 12345")
-  http.HandleFunc("/", HelloServer)
-  err := http.ListenAndServe(":12345", nil)
 
-  if err != nil {
-    log.Fatal("ListenAndServe: ", err)
-  }
+	log.Print("starting on 12345")
+	http.HandleFunc("/", HelloServer)
+	err := http.ListenAndServe(":12345", nil)
+
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
