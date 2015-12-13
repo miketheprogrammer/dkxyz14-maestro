28d5df26416cf48d5028e1e787d461898d5b1a25
Michael Hernandez <michael.hernandez1988@gmail.com>
2015-12-13 12:55:54 -0500

    add build images script, since some are not published to docker hub

---
+++ build_images.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker build -t miketheprogrammer/go-thrust-chat-server ./go-thrust
+docker build -t miketheprogrammer/hello-go ./hello-go
+docker build -t miketheprogrammer/hello-python . 
+docker build -t miketheprogrammer/hello-lua . 
